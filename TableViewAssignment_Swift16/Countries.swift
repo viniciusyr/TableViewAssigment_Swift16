8da6d637ea404e5c0b5d276ce4b7913ea85d7dcb
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2022-10-08 17:03:19 -0300

    Table View assignment using defaultSetings from swift 16

---
+++ TableViewAssignment_Swift16/Countries.swift
@@ -0,0 +1,74 @@
+//
+//  Countries.swift
+//  TableViewAssignment_Swift16
+//
+//  Created by Vinicius Rodrigues on 10/8/22.
+//
+
+import Foundation
+
+struct Countries {
+    
+    let name:[String] = ["China",
+                         "India",
+                         "United States",
+                         "Indonesia",
+                         "Pakistan",
+                         "Nigeria",
+                         "Brazil",
+                         "Bangladesh",
+                         "Russia",
+                         "Mexico",
+                         "Japan",
+                         "Ethiopia",
+                         "Philippines",
+                         "Egypt",
+                         "DR Congo",
+                         "Vietnam",
+                         "Iran",
+                         "Turkey",
+                         "Germany",
+                         "Thailand",
+                         "United Kingdom",
+                         "Tanzania",
+                         "France",
+                         "South Africa",
+                         "Italy",
+                         "England",
+                         "Myanmar",
+                         "Kenya",
+                         "Colombia",
+                         "South Korea"]
+    
+    let population:[String] = ["1425887.337",
+                                "1417173.173",
+                                "338289.857",
+                                "275501.339",
+                                "235824.862",
+                                "218541.212",
+                                "215313.498",
+                                "171186.372",
+                                "144713.314",
+                                "127504.125",
+                                "123951.692",
+                                "123379.924",
+                                "115559.009",
+                                "110990.103",
+                                "99010.212",
+                                "98186.856",
+                                "88550.57",
+                                "85341.241",
+                                "83369.843",
+                                "71697.03",
+                                "67508.936",
+                                "65497.748",
+                                "64626.628",
+                                "59893.885",
+                                "59037.474",
+                                "57367.811",
+                                "54179.306",
+                                "54027.487",
+                                "51874.024",
+                                "51815.81"]
+    
+}
--- TableViewAssignment_Swift16/ViewController.swift
+++ TableViewAssignment_Swift16/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //TableView
+    @IBOutlet var worldTableView: UITableView!
+    
+    // Identifier
+    let cellIdentifier = "CellID"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Register cell
+        worldTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        
     }
 
 
+}
+
+//Table setup
+extension ViewController: UITableViewDataSource {
+    //Number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Countries.init().name.count
+    }
+    
+    //Number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    //Content
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let country = Countries.init().name[indexPath.row]
+        let population = Countries.init().population[indexPath.row]
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = country
+        configuration.secondaryText = population
+        cell.contentConfiguration = configuration
+        
+        return cell
+        
+    }
+    
+    
 }
 
